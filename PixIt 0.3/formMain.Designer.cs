0adf516201260d746fcf9ee012cbf8e8e7128318
MichiHory <Mich.horak@email.cz>
2014-03-18 14:00:40 +0100

    Přepsání projektu
    Pix it 0.3

---
+++ PixIt 0.3/formMain.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace PixIt_0._3
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.btnPort = new System.Windows.Forms.Button();
+            this.btnManual = new System.Windows.Forms.Button();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.picOriginal = new System.Windows.Forms.PictureBox();
+            this.picDraw = new System.Windows.Forms.PictureBox();
+            this.listBoxVectors = new System.Windows.Forms.ListBox();
+            this.listBoxSerialPort = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.picOpenPort = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDraw)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOpenPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(14, 42);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Načíst";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(13, 71);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(75, 23);
+            this.btnSet.TabIndex = 1;
+            this.btnSet.Text = "Nastavení";
+            this.btnSet.UseVisualStyleBackColor = true;
+            // 
+            // btnPort
+            // 
+            this.btnPort.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnPort.Location = new System.Drawing.Point(14, 100);
+            this.btnPort.Name = "btnPort";
+            this.btnPort.Size = new System.Drawing.Size(75, 33);
+            this.btnPort.TabIndex = 2;
+            this.btnPort.Text = "Otevřít port";
+            this.btnPort.UseVisualStyleBackColor = true;
+            // 
+            // btnManual
+            // 
+            this.btnManual.Location = new System.Drawing.Point(14, 139);
+            this.btnManual.Name = "btnManual";
+            this.btnManual.Size = new System.Drawing.Size(75, 39);
+            this.btnManual.TabIndex = 3;
+            this.btnManual.Text = "Manuální ovládání";
+            this.btnManual.UseVisualStyleBackColor = true;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnDraw.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDraw.Location = new System.Drawing.Point(14, 293);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 49);
+            this.btnDraw.TabIndex = 4;
+            this.btnDraw.Text = "Vykreslit";
+            this.btnDraw.UseVisualStyleBackColor = false;
+            // 
+            // picOriginal
+            // 
+            this.picOriginal.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.picOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picOriginal.Location = new System.Drawing.Point(95, 42);
+            this.picOriginal.Name = "picOriginal";
+            this.picOriginal.Size = new System.Drawing.Size(350, 303);
+            this.picOriginal.TabIndex = 5;
+            this.picOriginal.TabStop = false;
+            // 
+            // picDraw
+            // 
+            this.picDraw.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.picDraw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picDraw.Location = new System.Drawing.Point(451, 42);
+            this.picDraw.Name = "picDraw";
+            this.picDraw.Size = new System.Drawing.Size(350, 303);
+            this.picDraw.TabIndex = 6;
+            this.picDraw.TabStop = false;
+            // 
+            // listBoxVectors
+            // 
+            this.listBoxVectors.FormattingEnabled = true;
+            this.listBoxVectors.Location = new System.Drawing.Point(807, 42);
+            this.listBoxVectors.Name = "listBoxVectors";
+            this.listBoxVectors.Size = new System.Drawing.Size(120, 303);
+            this.listBoxVectors.TabIndex = 7;
+            // 
+            // listBoxSerialPort
+            // 
+            this.listBoxSerialPort.FormattingEnabled = true;
+            this.listBoxSerialPort.Location = new System.Drawing.Point(807, 382);
+            this.listBoxSerialPort.Name = "listBoxSerialPort";
+            this.listBoxSerialPort.Size = new System.Drawing.Size(120, 108);
+            this.listBoxSerialPort.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Originál";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(458, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Zobrazení";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(804, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Vektory";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(804, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Data ze sériového portu";
+            // 
+            // picOpenPort
+            // 
+            this.picOpenPort.BackColor = System.Drawing.Color.Red;
+            this.picOpenPort.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picOpenPort.Location = new System.Drawing.Point(21, 124);
+            this.picOpenPort.Name = "picOpenPort";
+            this.picOpenPort.Size = new System.Drawing.Size(62, 8);
+            this.picOpenPort.TabIndex = 14;
+            this.picOpenPort.TabStop = false;
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 502);
+            this.Controls.Add(this.picOpenPort);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxSerialPort);
+            this.Controls.Add(this.listBoxVectors);
+            this.Controls.Add(this.picDraw);
+            this.Controls.Add(this.picOriginal);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.btnManual);
+            this.Controls.Add(this.btnPort);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "formMain";
+            this.Text = "PixIt 0.3";
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDraw)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOpenPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Button btnPort;
+        private System.Windows.Forms.Button btnManual;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.PictureBox picOriginal;
+        private System.Windows.Forms.PictureBox picDraw;
+        private System.Windows.Forms.ListBox listBoxVectors;
+        private System.Windows.Forms.ListBox listBoxSerialPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox picOpenPort;
+    }
+}
+
---
+++ PixIt 0.3/formSet.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace PixIt_0._3
+{
+    partial class formSet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRoute = new System.Windows.Forms.Button();
+            this.btnDrill = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.groupSetColor = new System.Windows.Forms.GroupBox();
+            this.btnTransition = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupSetColor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRoute
+            // 
+            this.btnRoute.Location = new System.Drawing.Point(65, 19);
+            this.btnRoute.Name = "btnRoute";
+            this.btnRoute.Size = new System.Drawing.Size(75, 23);
+            this.btnRoute.TabIndex = 0;
+            this.btnRoute.Text = "Cesta";
+            this.btnRoute.UseVisualStyleBackColor = true;
+            // 
+            // btnDrill
+            // 
+            this.btnDrill.Location = new System.Drawing.Point(65, 64);
+            this.btnDrill.Name = "btnDrill";
+            this.btnDrill.Size = new System.Drawing.Size(75, 23);
+            this.btnDrill.TabIndex = 1;
+            this.btnDrill.Text = "Vrtání";
+            this.btnDrill.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 120);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(68, 82);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(69, 13);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(68, 38);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(69, 13);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // groupSetColor
+            // 
+            this.groupSetColor.Controls.Add(this.btnTransition);
+            this.groupSetColor.Controls.Add(this.pictureBox4);
+            this.groupSetColor.Controls.Add(this.pictureBox1);
+            this.groupSetColor.Controls.Add(this.btnDrill);
+            this.groupSetColor.Controls.Add(this.btnRoute);
+            this.groupSetColor.Controls.Add(this.pictureBox2);
+            this.groupSetColor.Controls.Add(this.pictureBox3);
+            this.groupSetColor.Location = new System.Drawing.Point(12, 12);
+            this.groupSetColor.Name = "groupSetColor";
+            this.groupSetColor.Size = new System.Drawing.Size(147, 151);
+            this.groupSetColor.TabIndex = 7;
+            this.groupSetColor.TabStop = false;
+            this.groupSetColor.Text = "Nastavení barev";
+            // 
+            // btnTransition
+            // 
+            this.btnTransition.Location = new System.Drawing.Point(65, 108);
+            this.btnTransition.Name = "btnTransition";
+            this.btnTransition.Size = new System.Drawing.Size(75, 23);
+            this.btnTransition.TabIndex = 6;
+            this.btnTransition.Text = "Přechod";
+            this.btnTransition.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(68, 126);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(69, 13);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numPort);
+            this.groupBox1.Location = new System.Drawing.Point(166, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(133, 150);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nastavení COM portu";
+            // 
+            // numPort
+            // 
+            this.numPort.Location = new System.Drawing.Point(6, 21);
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(121, 20);
+            this.numPort.TabIndex = 0;
+            // 
+            // formSet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 175);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupSetColor);
+            this.Name = "formSet";
+            this.Text = "Nastavení";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupSetColor.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRoute;
+        private System.Windows.Forms.Button btnDrill;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.GroupBox groupSetColor;
+        private System.Windows.Forms.Button btnTransition;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numPort;
+    }
+}
